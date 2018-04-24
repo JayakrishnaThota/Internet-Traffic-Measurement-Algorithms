d73664b29c71570d8b1532fe5fe0af7411cb6152
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-04-24 14:54:35 -0400

    Create CountMin.java

---
+++ NetworkAlgosProject/src/CountMin.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+public class Project5 {
+	public static void main(String[] args)throws Exception {
+        int size = 1000000;
+        int[][] hash = new int[3][size];
+        Map<String, Integer> map = new HashMap<>();
+        File finput = new File("C:/Users/JayaKrishna/Desktop/Nad/project/traffic.txt");
+		File foutput = new File("C:/Users/JayaKrishna/Desktop/Nad/project/countmin.txt");
+		Scanner sc = null;
+		try
+		{
+			sc = new Scanner(finput);
+			sc.nextLine();
+			while(sc.hasNextLine())
+			{
+				String s = sc.nextLine();
+				String[] tokens = s.split("\\s+");
+				String key = tokens[0]+'$'+tokens[1];
+				int val = Integer.parseInt(tokens[2]);
+				map.put(key, map.getOrDefault(key,0)+val);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+	              new FileOutputStream(foutput), "utf-8"));
+		writer.write("Source Address		Destination Address			Estimate");
+		writer.newLine();
+
+        List<Integer> random = new ArrayList<>();
+        Random rand = new Random();
+        for(int i=0; i<99; i++)
+        {
+            random.add(rand.nextInt(Integer.MAX_VALUE)+1);
+        }
+
+        for (String key:map.keySet()) {
+            for(int i=0; i<3; i++)
+            {
+                int hashcode = Math.abs(key.hashCode());
+                int randomhashcode = random.get(i)^(hashcode);
+                int index = (randomhashcode)%size;
+                hash[i][index] += map.get(key);
+            }
+        }
+
+        for(String key:map.keySet())
+        {
+            int min = Integer.MAX_VALUE;
+            String[] tokens = key.split("\\$");
+            for(int i=0; i<3; i++)
+            {
+                int hashcode = Math.abs(key.hashCode());
+                int randomhash = random.get(i)^(hashcode);
+                int index = Math.abs(randomhash)%size;
+                min = Math.min(min, hash[i][index]);
+            }
+            writer.write(tokens[0]+"\t\t"+tokens[1]+"\t\t"+min);
+            writer.newLine();
+        }
+        writer.close();
+    }
+}
