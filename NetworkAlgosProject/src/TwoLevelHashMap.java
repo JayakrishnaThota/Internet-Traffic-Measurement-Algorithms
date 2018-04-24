89d297de94b69da69b19aa91196e683748a2a0da
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2018-04-24 13:44:51 -0400

    Update TwoLevelHashMap.java

--- NetworkAlgosProject/src/TwoLevelHashMap.java
+++ NetworkAlgosProject/src/TwoLevelHashMap.java
@@ -1,34 +1,48 @@
-import java.io.File;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
-public class TwoLevelHashMap {
+public class Project1 {
 	public static void main(String[] args){
-		File f = new File("C:/Users/JayaKrishna/Desktop/Nad/project/traffic.txt");
+		File finput = new File("C:/Users/JayaKrishna/Desktop/Nad/project/traffic.txt");
+		File foutput = new File("C:/Users/JayaKrishna/Desktop/Nad/project/two_level_hash_output.txt");
 		Map<String,Set<String>> map = new HashMap();
 		Scanner sc = null;
-		try{
-			sc = new Scanner(f);
+		try
+		{
+			sc = new Scanner(finput);
 			sc.nextLine();
-			while(sc.hasNextLine()){
+			while(sc.hasNextLine())
+			{
 				String s = sc.nextLine();
 				String[] tokens = s.split("\\s+");
 				String source = tokens[0];
 				String destination = tokens[1];
-				if(!map.containsKey(source)){
-					map.put(source, new HashSet());
+				if(!map.containsKey(source))
+				{
+					map.put(source, new HashSet<String>());
 				}
 				map.get(source).add(destination);
 			}
 		}
-		catch(Exception e){
+		catch(Exception e)
+		{
 			e.printStackTrace();
 		}
-		for(String key:map.keySet()){
-			System.out.println(key+"        "+map.get(key).size());
+
+
+		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+	              new FileOutputStream(foutput), "utf-8")))
+		{
+			writer.write("Source Address	No of unique destinations");
+			for(String key:map.keySet())
+			{
+				writer.write(key+"	"+map.get(key).size());
+				writer.newLine();
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
